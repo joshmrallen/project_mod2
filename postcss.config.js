d8965fcbd7eed145d886bd06815c4fb007bdf0aa
“Mineliegoma” <garlandegoma@gmail.com>
2020-08-20 18:24:20 -0400

    not showing

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('dotenv').config(),
+    console.log(process.env),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
